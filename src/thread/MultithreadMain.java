baa99fa21c5f220b6bef19bdaf100f2b12bdc886
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-06-07 14:18:40 +0000

    Add exception test.

--- src/thread/MultithreadMain.java
+++ src/thread/MultithreadMain.java
@@ -7,25 +7,38 @@ public class MultithreadMain {
         System.out.println("MULTITHREADING");
         System.out.println("To use multithreading, your class has to extends Thread class ");
         System.out.println("and override the method run(), also you have to call the method start() ");
-        System.out.println("because run() isn't multithreading.");
+        System.out.println("because run() isn't multithreading.");     
         System.out.println("___________________________________");
         
         System.out.println();
         System.out.println("Using run() method with two objects:");
         
-        MultithreadThing myThing = new MultithreadThing();
-        MultithreadThing myThing2 = new MultithreadThing();
+        MultithreadThing myThing = new MultithreadThing(1);
+        MultithreadThing myThing2 = new MultithreadThing(1);
         myThing.run();
         myThing2.run();
         
         
         System.out.println();
+        System.out.println("___________________________________");
         System.out.println("Using start() method with two objects:");
         
-        myThing.start();
-        myThing2.start();
+        MultithreadThing myThing3 = new MultithreadThing(1);
+        MultithreadThing myThing4 = new MultithreadThing(2);
+        myThing3.start();
+        myThing4.start();
         
+        System.out.println();
+        System.out.println();
+        System.out.println("___________________________________");
+        System.out.println("A interesting thing is, even on thread throws a Exception, the others threads");
+        System.out.println("will keep runing.");
+        System.out.println("___________________________________");
         
+        for(int i=0; i<=3; i++) {
+            MultithreadThing myThing5 = new MultithreadThing(i);
+            myThing5.start();
+        }
         
     }
 
--- src/thread/MultithreadThing.java
+++ src/thread/MultithreadThing.java
@@ -1,11 +1,27 @@
 package thread;
 
 public class MultithreadThing extends Thread{
+    
+    private int threadNumber;
+    
+    public MultithreadThing(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
 
     @Override
     public void run() {
         for(int i=1; i<=5; i++) {
-            System.out.println(i);
+            System.out.println(i + " of thread number: " + threadNumber);
+            
+            if(threadNumber == 3) {
+                throw new RuntimeException();
+            }
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            
         }
     }
 }
